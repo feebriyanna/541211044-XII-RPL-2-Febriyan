a8b376156f45820f48c013465f9c44a00eee5801
feebriyanna <febriyanoka08@gmail.com>
2023-11-16 08:32:19 +0700

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,72 @@
+const User = require('../models/User')
+
+module.exports ={
+    index : async (req, res) => {
+      try {
+          const users = await User.find()
+          if(users.length > 0){
+            res.status(200).json({
+                status: true,
+                data: users,
+                method: req.method,
+                url: req.url
+            })
+            
+          }else{
+              res.json({
+                  status: false,
+                  message: "Data Masih Kosong"
+              })
+          }
+         
+      } catch (error) {
+          res.status(400).json({sucess: false})
+        
+      }
+       
+      },
+      store: async (req, res) => {
+        try {
+            const user = await User.create(req.body)
+            res.status(200).json({
+                status: true,
+                data: user,
+                method:req.method,
+                url: req.url,
+                messege: "Data berhasil ditambahkan"
+            })
+        } catch (error) {
+            res.status(400).json({sucess: false, error: error.message})
+        }
+
+      },
+      update: (req, res) => {
+        const id = req.params.id
+        users.filter(user => {
+            if(user.id == id){
+                user.nama= req.body.nama
+                user.email= req.body.email
+                return user
+            }
+        })
+        res.json({
+            status: true,
+            data: users,
+            method:req.method,
+            url: req.url,
+            messege: "Data berhasil diubah"
+        })
+      },
+      delete: (req, res) => {
+        const id = req.params.id
+        users = users.filter(user => user.id != id)
+    
+        res.json({
+            status: true,
+            data: users,
+            method:req.method,
+            url: req.url,
+            messege: "Data berhasil dihapus"
+        })
+      }
+}
